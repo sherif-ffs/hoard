a180c103a435d8e51a45b046a624b32b00088575
Sherif Elmetwally <sge5@njit.edu>
2021-10-10 02:05:40 -0400

    add linting stuff

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,24 +1,26 @@
-const express = require('express');
-const app = express();
-const mongoose = require('mongoose');
-require('dotenv').config()
+import express = require('express');
+import mongoose, { ConnectOptions } from 'mongoose';
+require('dotenv').config();
 
-const username = "selmetwa";
-const cluster = "cluster0.eauvk";
-const dbname = "myFirstDatabase";
+const app = express();
+
+const username = 'selmetwa';
+const cluster = 'cluster0.eauvk';
+const dbname = 'myFirstDatabase';
 const password = process.env.PASSWORD;
 
 mongoose.connect(
-  `mongodb+srv://${username}:${password}@${cluster}.mongodb.net/${dbname}?retryWrites=true&w=majority`, 
+  `mongodb+srv://${username}:${password}@${cluster}.mongodb.net/${dbname}?retryWrites=true&w=majority`,
   {
     useNewUrlParser: true,
-    useUnifiedTopology: true
-  }
+    useUnifiedTopology: true,
+  } as ConnectOptions
 );
+
 const db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error: "));
-db.once("open", function () {
-  console.log("Connected successfully");
+db.on('error', console.error.bind(console, 'connection error: '));
+db.once('open', function () {
+  console.log('Connected successfully');
 });
 
 app.listen(5000, () => console.log('Server running'));
