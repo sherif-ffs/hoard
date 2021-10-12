fc0d8d97a8fcb1353aae91207f61d6d5b5898b5c
Sherif Elmetwally <sge5@njit.edu>
2021-10-11 23:00:13 -0400

    salt password

--- client/pages/index.tsx
+++ client/pages/index.tsx
@@ -1,7 +1,5 @@
 import type { NextPage } from 'next'
 import { useState } from 'react'
-import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
--- server/src/index.ts
+++ server/src/index.ts
@@ -4,6 +4,7 @@ import mongoose, { ConnectOptions } from 'mongoose';
 import path from 'path';
 import bodyParser = require('body-parser');
 const cors = require('cors');
+import bcrypt from 'bcryptjs';
 
 const app = express();
 
@@ -32,12 +33,13 @@ app.use(cors());
 app.use('/', express.static(path.join(__dirname, 'static')));
 
 app.post('/api/register', async (req, res) => {
-  console.log('req.body: ', req.body);
+  const { username, password } = req.body;
+
+  console.log('password: ', await bcrypt.hash(password, 10));
   res.json({ status: 'ok' });
 });
 
 import itemsRoute from './routes/itemsRoute';
-import testRouter from './testRoute';
 app.use('/items', itemsRoute);
 
 app.listen(5000, () => console.log('Server running'));
