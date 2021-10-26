26e9da9607ba6d5424f92622f0751cf0d28d0e83
Sherif Elmetwally <sge5@njit.edu>
2021-10-25 22:01:07 -0400

    render collections

--- client/pages/profile/[id].tsx
+++ client/pages/profile/[id].tsx
@@ -1,11 +1,45 @@
 import { useRouter } from 'next/router';
-import { useCollectionById } from '../hooks/useCollectionsById';
+import useCollectionById from '../hooks/useCollectionsById';
 
 const Profile = () => {
   const router = useRouter();
   const { id } = router.query;
+  let data, status, error;
+  if (id) {
+    const res = useCollectionById(id);
+    data = res.data;
+    status = res.status;
+    error = res.error;
+  }
 
-  return <p>User: {id}</p>;
+  console.log('data; ', data);
+  console.log('id; ', id);
+  console.log('status; ', status);
+  console.log('error; ', error);
+
+  const collectionsExist = !error && status === 'success' && data && data.data;
+  if (error) return <p>error!</p>;
+  if (status === 'loading' || !id) return <p>loading</p>;
+
+  if (collectionsExist) {
+    const collections = data.data;
+    return (
+      <section>
+        <p>User: {id}</p>
+        {collections &&
+          !!collections.length &&
+          collections.map((collection) => {
+            return (
+              <div key={collection._id}>
+                <p>Items: {collection.title}</p>
+                <p>Items: {collection.items.length}</p>
+              </div>
+            );
+            console.log('collection; ', collection);
+          })}
+      </section>
+    );
+  }
 };
 
 export default Profile;
