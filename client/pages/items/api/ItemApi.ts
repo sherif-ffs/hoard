4676226e27cfe72f6d2376c4efa71bd5dd4cdb6b
Sherif Elmetwally <sge5@njit.edu>
2022-01-15 21:35:38 -0500

    type stuff

--- client/pages/items/api/ItemApi.ts
+++ client/pages/items/api/ItemApi.ts
@@ -1,7 +1,7 @@
 import { ItemInterface } from '../../Interfaces/ItemInterface';
 
 // Create Item
-export function createItem(item: ItemInterface) {
+export function createItem(item: any) {
   return fetch('http://localhost:5000/items/create-item', {
     method: 'POST',
     credentials: 'include',
