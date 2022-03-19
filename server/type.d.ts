c31677a7bb429954957f59125d919e6b1deb53d2
sherif-ffs <46908343+sherif-ffs@users.noreply.github.com>
2022-03-19 16:45:41 -0400

    Delete server directory

--- server/type.d.ts
+++
@@ -1,9 +0,0 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-declare namespace Express {
-  export interface Request {
-    id: number;
-  }
-  export interface Response {
-    id: any;
-  }
-}
